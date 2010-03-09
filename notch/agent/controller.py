9fbae2708e14865cbb5945b556c023de7dc0c38b
Andrew Fort <andrew.fort@gmail.com>
2010-03-09 14:06:42 +1100

    Added session maintenance loop. Improve docstrings. Up maximum number of active sessions to 512.

--- notch/agent/controller.py
+++ notch/agent/controller.py
@@ -8,9 +8,11 @@ The controller manages device sessions and control flow handling for
 the RPC server. Routine maintenance activities are also located here.
 """
 
+import eventlet
 
 import collections
 import logging
+from eventlet.green import time
 
 import credential
 import device_factory
@@ -21,17 +23,11 @@ import notch_config
 import session
 
 
-
-# Maximum number of active sessions (held in LRU at any one time)
-MAX_ACTIVE_SESSIONS = 128
-
-
-class Error(Exception):
-    """Module-level exception."""
-
-
-class AddressLookupError(Error):
-    """There was an error during address lookup."""
+# Maximum number of active sessions (held in LRU at any one time).
+# TODO(afort): Make configurable.
+MAX_ACTIVE_SESSIONS = 512
+# Default session check window period in seconds.
+DEFAULT_SESSION_CHECK_PERIOD_S = 10.0
 
 
 class Controller(object):
@@ -45,14 +41,63 @@ class Controller(object):
     """
 
     def __init__(self, config=None):
+        """Initializer.
+
+        Args:
+          config: A dict, holding the configuration.
+        """
+        self.config = config or {}
+        self._get_timers_from_config(config)
         self.sessions = lru.LruDict(populate_callback=self.create_session,
                                     expire_callback=self.expire_session,
                                     maximum_size=MAX_ACTIVE_SESSIONS)
-        self.config = config or {}
         self.device_manager = device_manager.DeviceManager(self.config)
         self.load_credentials()
+        self._stopped = eventlet.event.Event()
+        self.__current_maint_thread = None
+
+    def stop(self):
+        self._stopped.send()
+        logging.debug('Disconnecting all sessions.')
+        for session in self.sessions.values():
+            session.disconnect()
+
+    def run_maintenance(self):
+        """Runs maintenance greenthreads."""
+        self._session_idle_check()
+        self._stopped.wait()
+
+    def _get_timers_from_config(self, config):
+        self._session_maint_period = DEFAULT_SESSION_CHECK_PERIOD_S
+        timers = self.config.get('timers')
+        if timers:
+            try:
+                self._session_maint_period = \
+                    float(timers.get('session_maint_period',
+                                     DEFAULT_SESSION_CHECK_PERIOD_S))
+            except ValueError:
+                pass
+
+    def _session_idle_check(self):
+        """Checks the idle timeouts for all sessions."""
+        start = time.time()
+        logging.debug('Checking idle sessions.')
+        for session in self.sessions.values():
+            if (not session.idle or not session.connected):
+                continue
+            if (time.time() > (session.time_last_request or 0) +
+                session.device.MAX_IDLE_TIME):
+                logging.debug('Session disconnect (idle): %s',
+                              session.device.name)
+                session.disconnect()
+        # Re-schedule ourself for execution.
+        elapsed = max(0, time.time() - start)
+        wait_time = max(0, self._session_maint_period - elapsed)
+        eventlet.spawn_after(
+            wait_time, self._session_idle_check)
 
     def load_credentials(self):
+        """Loads the credentials store (login passwords/keys)."""
         self.credentials = None
         options_section = self.config.get('options')
         if not options_section:
@@ -64,7 +109,7 @@ class Controller(object):
         else:
             logging.debug('Loading credentials from file %r', creds_filename)
             self.credentials = credential.load_credentials_file(creds_filename)
-        
+
     def create_session(self, key):
         """Creates a session.Session object for the session key.
 
@@ -122,7 +167,7 @@ class Controller(object):
             return None
 
     def request(self, method, **kwargs):
-        """Executes a request.
+        """Executes a Notch device API request.
 
         Args:
           method: A string, the device API method name.
@@ -138,10 +183,12 @@ class Controller(object):
             raise errors.NoSessionCreatedError(
                 'No session available for request arguments %r' % kwargs)
 
-        if self.credentials and 'device_name' in kwargs:
-            session.credential = self.credentials.get_credential(
-                kwargs['device_name'])
         try:
+            # TODO(afort): What if there's no credentials or device name is
+            # missing? raise an invalid request error response to the user.
+            if self.credentials and 'device_name' in kwargs:
+                session.credential = self.credentials.get_credential(
+                    kwargs['device_name'])
             return session.request(method, **kwargs)
         except errors.ApiError, e:
             # TODO(afort): API errors cause an error response over the wire.
