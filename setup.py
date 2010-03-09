c0aeb9c609650c1c62b98bfb75212404e70b966d
Andrew Fort <andrew.fort@gmail.com>
2010-03-10 03:42:15 +1100

    Add tornado package requirement

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setuptools.setup(
                  ],
     packages = setuptools.find_packages(),
     install_requires=['distribute'],
-    requires=['eventlet>=0.9.6'],
+    requires=['eventlet>=0.9.6', 'tornado>=0.2'],
     )
