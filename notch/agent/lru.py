2c41f7874603de0dfb9f69a6a80c98aab3ac6664
Andrew Fort <andrew.fort@gmail.com>
2010-03-09 19:55:27 +1100

    Add Apache License message to each Python sourcefile.

--- notch/agent/lru.py
+++ notch/agent/lru.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 #
 # Copyright 2009 Andrew Fort. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 """A callback-populated least-recently used cache that behaves like a dict."""
 
@@ -8,6 +20,7 @@ import copy
 import heapq
 import time
 import UserDict
+
 
 # pylint:disable-msg=R0903
 class HeapItem(object):
