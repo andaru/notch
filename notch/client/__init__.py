b981b7756e0b48636c69fc8f5133155ce0d7f987
Andrew Fort <amf@enemesco.net>
2011-01-14 12:44:39 +1100

    Remove code now in notch.client module

--- notch/client/__init__.py
+++
@@ -1,6 +0,0 @@
-from client import (Error, NoAgentsError, NoCallbackError, UnknownCommandError,
-                    Connection, Request, TimeoutError, RequestCancelledError)
-
-
-__all__ = ['Error', 'NoAgentsError', 'NoCallbackError', 'UnknownCommandError',
-           'Connection', 'Request', 'TimeoutError', 'RequestCancelledError']
