17001eaa26b8f8f4214c31b2f726302b19784186
Andrew Fort <amf@enemesco.net>
2011-01-20 14:17:36 +1100

    Roll version number to 0.4.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 
 setuptools.setup(
     name='notch',
-    version='0.4.2',
+    version='0.4.3',
     description='The Network Operator\'s Toolkit for Command-line Hacking',
     entry_points = {
         'console_scripts': [
